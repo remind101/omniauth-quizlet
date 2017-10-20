0fbafbee8cbb77188f6f3d1cd5416a33df928c89
Peter Hamilton <peterghamilton@gmail.com>
2017-10-20 10:37:05 -0700

    Remove extra and raw_info
    Making the second call was broken, and it didn't give us anything new anyway.

--- lib/omniauth/strategies/quizlet.rb
+++ lib/omniauth/strategies/quizlet.rb
@@ -17,36 +17,8 @@ module OmniAuth
         :scope => 'read',
         :state => 'some_random_string'
       }
-      
-      extra do
-        raw_info
-      end
 
-      uid { raw_info['user_id'] }
-
-      def raw_info
-        access_token.options[:mode] = :header
-				encoded_auth = Base64.encode64("#{options.client_id}:#{options.client_secret}").strip
-        access_token.options[:header_format] = "Basic #{encoded_auth}"
-        access_token.options[:param_name] = :code
-        access_token.options[:headers] = {
-          'grant_type' => 'authorization_code'
-        }
-        begin
-          ops = {
-            :headers => { 'grant_type'=>'authorization_code'},
-            :body => { 
-              'grant_type'=>'authorization_code',
-              'code' => @request.params['code']
-            }
-          }
-          @raw_info ||= access_token.post(options.client_options['token_url'],ops).parsed
-        rescue Exception => e
-          Rails.logger.info e.inspect
-        end
-      end 
-
-
+      uid { access_token.params['user_id'] }
     end
   end
 end
